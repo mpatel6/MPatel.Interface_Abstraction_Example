4de3fbf263a88271bd281f1c4af391ec549f5146
mpatel6 <mpatel6@L11105042790.wctc.msft>
2015-01-26 17:00:17 -0600

    InterfaceExample

---
+++ src/real_inheritance_abstraction_examples/Automobile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package real_inheritance_abstraction_examples;
+
+/**
+ *
+ * @author Ankita
+ */
+public class Automobile extends Vehicle {
+
+    private String carManufacturer;
+
+    public String getCarManufacturer() {
+        return carManufacturer;
+    }
+
+    public void setCarManufacturer(String carManufacturer) {
+        this.carManufacturer = carManufacturer;
+    }
+
+}
